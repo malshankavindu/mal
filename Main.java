05e0620af980f49f9202b549cb86d89e3151ff44
malshankavindu <malshankavindu432@gmail.com>
2026-03-04 18:31:00 +0530

    new2

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package mal;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("Hello,world!");
+        System.err.println("Hello,world");
     }
     
 }
